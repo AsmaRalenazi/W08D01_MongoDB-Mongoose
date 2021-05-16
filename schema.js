28cf52637e8066597478d4ba22e4f33028ee8e4d
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-16 13:45:30 +0300

    Pulse Check q3 & q4

--- schema.js
+++ schema.js
@@ -1,1 +1,13 @@
 const mongoose= require ("mongoose");
+
+const todoSchema =new mongoose.Schema({
+
+    task:{type:String, required:true},
+    description:{type:String, required:true},
+    deadline :{type:String, required:true},
+    isCompleted:{type:String, required:true},
+    priority:{type:String, }
+})
+
+module.exports=mongoose.model("todo",todoSchema)
+//module.exports = mongoose.model("User",userSchema)
